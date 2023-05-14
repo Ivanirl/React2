08cdca4c57f30bcbad982442f8c563c8fb3a8d74
Ivan <ivanezeh12@gmail.com>
2023-05-14 09:20:15 +0100

    5

---
+++ src/Components/ToDo.js
@@ -0,0 +1,12 @@
+function ToDo() {
+  return (
+    <div className="card">
+      <h2>Learn React</h2>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default ToDo;
