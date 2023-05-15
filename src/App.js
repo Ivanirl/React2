32bd46b615577098c15d0ac2c3ea49608bce6023
Ivan <ivanezeh12@gmail.com>
2023-05-15 10:35:53 +0100

    hm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import ToDo from './Components/ToDo'
-import Modal from './Components/Modal'
-import Backdrop from './Components/backdrop';
-
 
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <ToDo text='Learn React'/>
-      <ToDo text='Learn Python'/>
-      <ToDo text='Learn C++'/>
-      <ToDo text='Learn Java'/>
-      <Modal />
-      <Backdrop />
-    </div>
+    <div></div>
   );
 }
 
