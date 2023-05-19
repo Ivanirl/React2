263c549c8ed940fc8357782d3dd740c740bb325d
Ivan <ivanezeh12@gmail.com>
2023-05-19 03:14:33 +0100

    sprry for the delay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route } from "react-router-dom";
+
+import AllMeetUpsPage from "./pages/AllMeetups";
+import NewMeetUpsPage from "./pages/NewMeetups";
+import FavouritesPage from "./pages/favourites";
+
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Route path="/">
+        <AllMeetUpsPage />
+      </Route>
+      <Route path="/new-meetups">
+        <NewMeetUpsPage />
+      </Route>
+      <Route path="/favourites">
+        <FavouritesPage />
+      </Route>
+    </div>
   );
 }
 
